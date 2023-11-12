141cc8bd2388f0ede50424f7241bae317c988c2d
Fernando <pinto.faa@gmail.com>
2023-11-12 17:31:36 -0300

    + Gateway service update to use product_rpc.get(id)

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -93,17 +93,15 @@ class GatewayService(object):
         # raise``OrderNotFound``
         order = self.orders_rpc.get_order(order_id)
 
-        # Retrieve all products from the products service
-        product_map = {prod['id']: prod for prod in self.products_rpc.list()}
-
         # get the configured image root
         image_root = config['PRODUCT_IMAGE_ROOT']
 
         # Enhance order details with product and image details.
         for item in order['order_details']:
             product_id = item['product_id']
-
-            item['product'] = product_map[product_id]
+            # Retrieve one product from the products service
+            product = self.products_rpc.get(product_id)
+            item['product'] = product
             # Construct an image url.
             item['image'] = '{}/{}.jpg'.format(image_root, product_id)
 
